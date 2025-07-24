5ebe40c2acfa5a1397970b3dcf7ceab28c8307d2
Pavan <pavan.alpha.omega@gmail.com>
2025-07-24 18:40:13 +0100

    refactor (test_mathematical): reorder test functions -- no functional changes

--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -18,6 +18,120 @@ import mathematical as math
 import simulation as sim
 
 
+def test_rescale_matrix() -> None:
+    """
+    Test that the "rescale_matrix" function performs as expected.
+    """
+
+    print("Function Tested: src.mathematical.rescale_matrix")
+    print("------------------------------------------------")
+
+    # Construct a simple matrix.
+    diagonal: sim.RVector = np.linspace(-200, 200, 20, dtype=np.float64)
+    matrix: sim.RMatrix = np.diag(diagonal)
+
+    # Define the target interval.
+    a: float = -1.0
+    b: float = 1.0
+
+    # Rescale the eigenvalue domain of the matrix.
+    matrix_rs, scale, shift = math.rescale_matrix(matrix, a, b)
+
+    # Check that the rescaled eigenvalue domain is as expected.
+    eigenvalues_rs: sim.RVector = np.linalg.eigvalsh(matrix_rs).astype(np.float64)
+    print(eigenvalues_rs)
+
+    assert np.isclose(np.min(eigenvalues_rs), a)
+    assert np.isclose(np.max(eigenvalues_rs), b)
+
+    # Check that the scale and shift factors perform the correct inverse transformation.
+    matrix_original: sim.GMatrix = (
+        matrix_rs - (shift * np.identity(matrix.shape[0], dtype=matrix.dtype))
+    ) / scale
+    assert np.allclose(matrix, matrix_original)
+
+    print("Passed")
+    print("------")
+
+
+def test_rescale_vector() -> None:
+    """
+    Test that the "rescale_vector" function performs as expected.
+    """
+
+    print("Function Tested: src.mathematical.rescale_vector")
+    print("------------------------------------------------")
+
+    # Construct a simple vector.
+    vector: sim.RVector = np.linspace(-200, 200, 20, dtype=np.float64)
+
+    # Define the target interval.
+    a: float = -1.0
+    b: float = 1.0
+
+    # Rescale the domain of the vector.
+    vector_rs, scale, shift = math.rescale_vector(vector, a, b)
+
+    # Check that the rescaled vector is as expected.
+    print(vector_rs)
+
+    assert np.isclose(np.min(vector_rs), a)
+    assert np.isclose(np.max(vector_rs), b)
+
+    # Check that the scale and shift factors perform the correct inverse transformation.
+    vector_original: sim.RVector = (vector_rs - shift) / scale
+    assert np.allclose(vector, vector_original)
+
+    print("Passed")
+    print("------")
+
+
+def test_ch_gauss_nodes() -> None:
+    """
+    Test that the "ch_gauss_nodes" function performs as expected.
+    """
+
+    print("Function Tested: src.mathematical.ch_gauss_nodes")
+    print("------------------------------------------------")
+
+    # Check that the Chebyshev-Gauss nodes are generated as expected.
+    num_nodes: int = 20
+    nodes: sim.RVector = math.ch_gauss_nodes(num_nodes)
+
+    # Print the Chebyshev-Gauss nodes.
+    print(nodes)
+
+    # Plot the Chebyshev-Gauss nodes.
+    plt.plot(nodes)
+    plt.show()
+
+    print("Passed")
+    print("------")
+
+
+def test_ch_lobatto_nodes() -> None:
+    """
+    Test that the "ch_lobatto_nodes" function performs as expected.
+    """
+
+    print("Function Tested: src.mathematical.ch_lobatto_nodes")
+    print("--------------------------------------------------")
+
+    # Check that the Chebyshev-Lobatto nodes are generated as expected.
+    num_nodes: int = 20
+    nodes: sim.RVector = math.ch_lobatto_nodes(num_nodes)
+
+    # Print the Chebyshev-Lobatto nodes.
+    print(nodes)
+
+    # Plot the Chebyshev-Lobatto nodes.
+    plt.plot(nodes)
+    plt.show()
+
+    print("Passed")
+    print("------")
+
+
 def test_ch_coefficients() -> None:
     """
     Test that the "ch_coefficients" function performs as expected.
@@ -157,120 +271,6 @@ def test_ch_expansion() -> None:
     plt.plot(domain.x_axis, expansion, linestyle="--", label="Expansion")
     plt.legend()
     plt.show()
-
-    print("Passed")
-    print("------")
-
-
-def test_ch_gauss_nodes() -> None:
-    """
-    Test that the "ch_gauss_nodes" function performs as expected.
-    """
-
-    print("Function Tested: src.mathematical.ch_gauss_nodes")
-    print("------------------------------------------------")
-
-    # Check that the Chebyshev-Gauss nodes are generated as expected.
-    num_nodes: int = 20
-    nodes: sim.RVector = math.ch_gauss_nodes(num_nodes)
-
-    # Print the Chebyshev-Gauss nodes.
-    print(nodes)
-
-    # Plot the Chebyshev-Gauss nodes.
-    plt.plot(nodes)
-    plt.show()
-
-    print("Passed")
-    print("------")
-
-
-def test_ch_lobatto_nodes() -> None:
-    """
-    Test that the "ch_lobatto_nodes" function performs as expected.
-    """
-
-    print("Function Tested: src.mathematical.ch_lobatto_nodes")
-    print("--------------------------------------------------")
-
-    # Check that the Chebyshev-Lobatto nodes are generated as expected.
-    num_nodes: int = 20
-    nodes: sim.RVector = math.ch_lobatto_nodes(num_nodes)
-
-    # Print the Chebyshev-Lobatto nodes.
-    print(nodes)
-
-    # Plot the Chebyshev-Lobatto nodes.
-    plt.plot(nodes)
-    plt.show()
-
-    print("Passed")
-    print("------")
-
-
-def test_rescale_matrix() -> None:
-    """
-    Test that the "rescale_matrix" function performs as expected.
-    """
-
-    print("Function Tested: src.mathematical.rescale_matrix")
-    print("------------------------------------------------")
-
-    # Construct a simple matrix.
-    diagonal: sim.RVector = np.linspace(-200, 200, 20, dtype=np.float64)
-    matrix: sim.RMatrix = np.diag(diagonal)
-
-    # Define the target interval.
-    a: float = -1.0
-    b: float = 1.0
-
-    # Rescale the eigenvalue domain of the matrix.
-    matrix_rs, scale, shift = math.rescale_matrix(matrix, a, b)
-
-    # Check that the rescaled eigenvalue domain is as expected.
-    eigenvalues_rs: sim.RVector = np.linalg.eigvalsh(matrix_rs).astype(np.float64)
-    print(eigenvalues_rs)
-
-    assert np.isclose(np.min(eigenvalues_rs), a)
-    assert np.isclose(np.max(eigenvalues_rs), b)
-
-    # Check that the scale and shift factors perform the correct inverse transformation.
-    matrix_original: sim.GMatrix = (
-        matrix_rs - (shift * np.identity(matrix.shape[0], dtype=matrix.dtype))
-    ) / scale
-    assert np.allclose(matrix, matrix_original)
-
-    print("Passed")
-    print("------")
-
-
-def test_rescale_vector() -> None:
-    """
-    Test that the "rescale_vector" function performs as expected.
-    """
-
-    print("Function Tested: src.mathematical.rescale_vector")
-    print("------------------------------------------------")
-
-    # Construct a simple vector.
-    vector: sim.RVector = np.linspace(-200, 200, 20, dtype=np.float64)
-
-    # Define the target interval.
-    a: float = -1.0
-    b: float = 1.0
-
-    # Rescale the domain of the vector.
-    vector_rs, scale, shift = math.rescale_vector(vector, a, b)
-
-    # Check that the rescaled vector is as expected.
-    print(vector_rs)
-
-    assert np.isclose(np.min(vector_rs), a)
-    assert np.isclose(np.max(vector_rs), b)
-
-    # Check that the scale and shift factors perform the correct inverse transformation.
-    vector_original: sim.RVector = (vector_rs - shift) / scale
-    assert np.allclose(vector, vector_original)
 
     print("Passed")
     print("------")
