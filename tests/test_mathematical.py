a39c3c26055603e8a5d1a93078fe78f4b7628563
Pavan <pavan.alpha.omega@gmail.com>
2025-07-24 16:26:56 +0100

    test: update test case for the ChebYshev coefficients function

--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -28,7 +28,7 @@ def test_ch_coefficients() -> None:
 
     # Define a known function.
     def function(x: sim.RVector) -> sim.RVector:
-        return x**2
+        return x**3
 
     # Define the Chebyshev expansion.
     def ch_expansion(nodes: sim.RVector, coefficients: sim.RVector) -> sim.RVector:
@@ -62,16 +62,18 @@ def test_ch_coefficients() -> None:
 
     function_nodes: sim.RVector = function(nodes)
     function_coefficients: sim.RVector = math.ch_coefficients(
-        function_nodes, type=2
+        function_nodes[::-1], type=2
     ).astype(np.float64)
     function_expansion: sim.RVector = ch_expansion(nodes, function_coefficients)
 
     # Check that the coefficients are as expected.
-    assert np.isclose(function_coefficients[0], 0.5)
-    assert np.isclose(function_coefficients[2], 0.5)
+    print(function_coefficients)
 
-    mask: NDArray[np.bool] = np.ones(order, dtype=np.bool)
-    mask[[0, 2]] = False
+    assert np.isclose(function_coefficients[1], 0.75)
+    assert np.isclose(function_coefficients[3], 0.25)
+
+    mask: NDArray[np.bool_] = np.ones(order, dtype=np.bool_)
+    mask[[1, 3]] = False
     assert np.allclose(function_coefficients[mask], 0.0)
 
     # Plot the exact and approximate solutions.
@@ -85,16 +87,18 @@ def test_ch_coefficients() -> None:
 
     function_nodes: sim.RVector = function(nodes)
     function_coefficients: sim.RVector = math.ch_coefficients(
-        function_nodes, type=1
+        function_nodes[::-1], type=1
     ).astype(np.float64)
     function_expansion: sim.RVector = ch_expansion(nodes, function_coefficients)
 
     # Check that the coefficients are as expected.
-    assert np.isclose(function_coefficients[0], 0.5)
-    assert np.isclose(function_coefficients[2], 0.5)
+    print(function_coefficients)
 
-    mask: NDArray[np.bool] = np.ones(order, dtype=np.bool)
-    mask[[0, 2]] = False
+    assert np.isclose(function_coefficients[1], 0.75)
+    assert np.isclose(function_coefficients[3], 0.25)
+
+    mask: NDArray[np.bool_] = np.ones(order, dtype=np.bool_)
+    mask[[1, 3]] = False
     assert np.allclose(function_coefficients[mask], 0.0)
 
     # Plot the exact and approximate solutions.
@@ -207,8 +211,8 @@ def test_rescale_vector() -> None:
 
 if __name__ == "__main__":
     # Run test cases.
-    test_ch_coefficients()
-    test_ch_gauss_nodes()
-    test_ch_lobatto_nodes()
     test_rescale_matrix()
     test_rescale_vector()
+    test_ch_gauss_nodes()
+    test_ch_lobatto_nodes()
+    test_ch_coefficients()
