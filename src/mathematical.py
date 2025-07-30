fix (src): fix calculation and application of conversion matrix -- Semi-Global propagation scheme working as intended [working]

src/mathematical.py
@@ -321,6 +321,9 @@ def ne_ta_conversion(time_axis: sim.RVector) -> sim.RMatrix:
     # Store the number of expansion terms.
     order: int = time_axis.shape[0]
 
+    # Account for the length four domain.
+    domain_factor: float = 4.0 / (time_axis[-1] - time_axis[0])
+
     # Set up the conversion matrix.
     conversion: sim.RMatrix = np.zeros((order, order), dtype=np.float64)
     conversion[0, 0] = 1.0
@@ -328,20 +331,16 @@ def ne_ta_conversion(time_axis: sim.RVector) -> sim.RMatrix:
     # Construct the complete matrix.
     for i in range(1, order):
         # Calculate the m = 0 term (Semi-Global Appendix C.1).
-        conversion[i, 0] = -time_axis[i - 1] * conversion[i - 1, 0]
+        conversion[i, 0] = -domain_factor * time_axis[i - 1] * conversion[i - 1, 0]
 
         # Calculate the 1 <= m <= n - 1 terms (Semi-Global Appendix C.1).
         for j in range(1, i):
-            conversion[i, j] = conversion[i - 1, j - 1] - (
-                time_axis[i - 1] * conversion[i - 1, j]
+            conversion[i, j] = domain_factor * (
+                conversion[i - 1, j - 1] - (time_axis[i - 1] * conversion[i - 1, j])
             )
 
         # Calculate the m = n term (Semi-Global Appendix C.1).
-        conversion[i, i] = conversion[i - 1, i - 1]
-
-    # Account for a length four domain.
-    conversion *= 4 / (time_axis[-1] - time_axis[0])
-    conversion[0, 0] = 1.0
+        conversion[i, i] = domain_factor * conversion[i - 1, i - 1]
 
     return conversion
 